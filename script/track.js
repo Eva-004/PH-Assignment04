8c4e7625a97994fd26237be371038ab9b5e8f1c9
Eva-004 <foredajannateva@gmail.com>
2026-02-23 00:27:42 +0600

    Fix some functionality

--- script/track.js
+++ script/track.js
@@ -1,10 +1,11 @@
 let interviewList = [];
 let rejectedList = [];
-let currentStatus = 'all';
+let currentStatus = 'all-btn';
 
 let total = document.getElementById('total');
 let interviewCount = document.getElementById('interviewCount');
 let rejectCount = document.getElementById('rejectCount');
+let showJobs = document.getElementById('showJobs');
 
 const allBtn = document.getElementById('all-btn');
 const interviewBtn = document.getElementById('interview-btn');
@@ -13,7 +14,7 @@ const rejectBtn = document.getElementById('reject-btn');
 const allJobs = document.getElementById('all-jobs');
 const mainContainer = document.querySelector('main');
 const filteredJobs = document.getElementById('filtered-jobs');
-
+showJobs.innerText = allJobs.children.length + ' Jobs';
 function calculateCount() {
     total.innerText = allJobs.children.length;
     interviewCount.innerText = interviewList.length;
@@ -37,14 +38,28 @@ function toggleStyle(id) {
     if(id == 'interview-btn'){
         allJobs.classList.add('hidden');
         filteredJobs.classList.remove('hidden');
-        renderInterview();
+        if(interviewList.length == 0){
+            renderNoInterview();
+              showJobs.innerText = '0 Jobs';
+        }else{
+            renderInterview();
+              showJobs.innerText = interviewList.length + ' of ' + allJobs.children.length + ' Jobs';
+        }
+      
     }else if(id == 'all-btn'){
           allJobs.classList.remove('hidden');
         filteredJobs.classList.add('hidden');
+        showJobs.innerText = allJobs.children.length + ' Jobs';
     }else if(id == 'reject-btn'){
          allJobs.classList.add('hidden');
         filteredJobs.classList.remove('hidden');
-        renderRejected();
+        if(rejectedList.length == 0){
+            renderNoReject();
+            showJobs.innerText = '0 Jobs';
+        }else{
+            renderRejected();
+            showJobs.innerText = rejectedList.length + ' of ' + allJobs.children.length + ' Jobs';
+        } 
     }
 }
 
@@ -68,7 +83,7 @@ mainContainer.addEventListener('click', function (event) {
             jobsStatus:'Interview',
             jobsDescription
         }
-
+        
         const jobExists = interviewList.find(item => item.jobsName == jobsInfo.jobsName);
         
         if (!jobExists) {
@@ -76,9 +91,16 @@ mainContainer.addEventListener('click', function (event) {
         }
         rejectedList = rejectedList.filter(item=> item.jobsName!=jobsInfo.jobsName);
         if(currentStatus=='reject-btn'){
-              renderRejected();
-          }
+              if(rejectedList.length == 0){
+            renderNoReject();
+           showJobs.innerText = '0 Jobs';
+        }else{
+            renderRejected();
+             showJobs.innerText = rejectedList.length + ' of ' + allJobs.children.length + ' Jobs';
+        }
+         }
         calculateCount();
+        
     }else if (event.target.classList.contains('rejected-btn')) {
         const parentNode = event.target.parentNode.parentNode;
 
@@ -105,10 +127,15 @@ mainContainer.addEventListener('click', function (event) {
         }
           interviewList= interviewList.filter(item=> item.jobsName!=jobsInfo.jobsName);
           if(currentStatus=='interview-btn'){
-              renderInterview();
+              if(interviewList.length == 0){
+            renderNoInterview();
+            showJobs.innerText = '0 Jobs';
+        }else{
+            renderInterview();
+             showJobs.innerText = interviewList.length + ' of ' + allJobs.children.length + ' Jobs';
+        }
           }
-        calculateCount();
-        
+        calculateCount(); 
     }
 });
 
@@ -121,14 +148,12 @@ function renderInterview() {
                    <div class="card-body space-y-5">
                         <div class="flex-col space-y-1">
                             <h2 class="jobs-name card-title text-[#002C5C]">${interview.jobsName}</h2>
-                            <p class="jobs-position text-[#64748B]">React Native Developer</p>
+                            <p class="jobs-position text-[#64748B]">${interview.jobsPosition}</p>
                         </div>
-                        <p class="jobs-salary text-[#64748B]">Remote • Full-time • $130,000 - $175,000</p>
+                        <p class="jobs-salary text-[#64748B]">${interview.jobsSalary}</p>
                         <div class="space-y-2">
                             <div id="status" class="jobs-status py-2 px-3 bg-[#EEF4FF] w-28 rounded-sm font-medium text-[#002C5C] text-center">${interview.jobsStatus}</div>
-                            <p class="jobs-description text-[#323B49]">Build cross-platform mobile applications using React Native. Work
-                                on
-                                products used by millions of users worldwide.</p>
+                            <p class="jobs-description text-[#323B49]">${interview.jobsDescription}</p>
                         </div>
                         <div class="flex gap-2">
                             <button class="interview-btn btn btn-outline btn-accent">Interview</button>
@@ -142,6 +167,7 @@ function renderInterview() {
         `
         filteredJobs.appendChild(div);
     }
+  
 }
 function renderRejected() {
     filteredJobs.innerHTML = '';
@@ -152,14 +178,12 @@ function renderRejected() {
                    <div class="card-body space-y-5">
                         <div class="flex-col space-y-1">
                             <h2 class="jobs-name card-title text-[#002C5C]">${reject.jobsName}</h2>
-                            <p class="jobs-position text-[#64748B]">React Native Developer</p>
+                            <p class="jobs-position text-[#64748B]">${reject.jobsPosition}</p>
                         </div>
-                        <p class="jobs-salary text-[#64748B]">Remote • Full-time • $130,000 - $175,000</p>
+                        <p class="jobs-salary text-[#64748B]">${reject.jobsSalary}</p>
                         <div class="space-y-2">
                             <div id="status" class="jobs-status py-2 px-3 bg-[#EEF4FF] w-28 rounded-sm font-medium text-[#002C5C] text-center">${reject.jobsStatus}</div>
-                            <p class="jobs-description text-[#323B49]">Build cross-platform mobile applications using React Native. Work
-                                on
-                                products used by millions of users worldwide.</p>
+                            <p class="jobs-description text-[#323B49]">${reject.jobsDescription}</p>
                         </div>
                         <div class="flex gap-2">
                             <button class="interview-btn btn btn-outline btn-accent">Interview</button>
@@ -174,3 +198,41 @@ function renderRejected() {
         filteredJobs.appendChild(div);
     }
 }
+
+function renderNoInterview() {
+    filteredJobs.innerHTML = '';
+    
+        let div = document.createElement('div');
+        div.className = 'flex card w-[100%] bg-base-100 card-md shadow flex-row';
+        div.innerHTML = `
+                   <div class="card-body space-y-2 text-center">
+                        <div class="flex justify-center items-center">
+                        <i class="fa-solid fa-file-lines text-4xl text-gray-400"></i>
+                        </div>
+                        <h3 class="text-[#002C5C] font-bold">No jobs available</h3>
+                        <p class="text-[#64748B]">Check back soon for new job opportunities</p>
+                    </div>
+                   `
+        filteredJobs.appendChild(div);
+     
+}
+
+function renderNoReject() {
+    filteredJobs.innerHTML = '';
+   
+        let div = document.createElement('div');
+        div.className = 'flex card w-[100%] bg-base-100 card-md shadow flex-row';
+        div.innerHTML = `
+                   <div class="card-body space-y-2 text-center">
+                        <div class="flex justify-center items-center">
+                        <i class="fa-solid fa-file-lines text-4xl text-gray-400"></i>
+                        </div>
+                        <h3 class="text-[#002C5C] font-bold">No jobs available</h3>
+                        <p class="text-[#64748B]">Check back soon for new job opportunities</p>
+                    </div>
+        `
+        filteredJobs.appendChild(div);
+     
+}
+
+
